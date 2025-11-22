a9ea67d3c646a546efacce128900e4f40e5e72b0
Cidra Hamidaddin <cidrahamidaddin@Cidras-MacBook-Air.local>
2025-11-22 03:53:53 +0300

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-task/"   // ← this MUST match your repo name EXACTLY
+  base: "/react task/"   // ← this MUST match your repo name EXACTLY
 })
